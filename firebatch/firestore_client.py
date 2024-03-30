84eaef0ffbc3f049f81b9166f76566bf8094ca68
Alexander Balasch <bll@tgw-group.com>
2024-03-30 19:25:12 +0000

    improve error message if not authenticated

--- firebatch/firestore_client.py
+++ firebatch/firestore_client.py
@@ -7,8 +7,14 @@ def initialize_firestore_client():
         db = firestore.Client()
         return db
     except Exception as e:
-        print("Failed to initialize Firestore client:", str(e))
-        print("Please ensure you are authenticated. You can do this by running 'gcloud auth application-default login --no-launch-browser',")
-        print("or by setting the GOOGLE_APPLICATION_CREDENTIALS environment variable.")
-        print("More info: https://cloud.google.com/docs/authentication/application-default-credentials")
+        error_message = (
+            f"Failed to initialize Firestore client: {str(e)}\n"
+            "Please ensure you are authenticated. You can do this by running:\n"
+            "'gcloud auth login --no-launch-browser' (if you are not logged in already)\n"
+            "and then:\n"
+            "'gcloud auth application-default login --no-launch-browser',\n"
+            "or by setting the GOOGLE_APPLICATION_CREDENTIALS environment variable.\n"
+            "More info: https://cloud.google.com/docs/authentication/application-default-credentials\n"
+        )
+        sys.stderr.write(error_message)
         sys.exit(1)
